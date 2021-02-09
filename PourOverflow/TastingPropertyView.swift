0f0fe725c7bec48be769d6e23858e0a8865afac9
Martin Granados Garcia <margrangr@gmail.com>
2021-02-09 08:26:35 -0600

    Add multiple IBinteractable outlets for the TastingProperty View

--- PourOverflow/TastingPropertyView.swift
+++ PourOverflow/TastingPropertyView.swift
@@ -10,13 +10,77 @@ import UIKit
 @IBDesignable
 class TastingPropertyView: UIView {
     @IBOutlet var contentView: UIView!
-    @IBOutlet var propertyTitle: UILabel!
+    @IBOutlet weak var propertyTitle: UILabel!
 
-    @IBOutlet var firstLabel: UILabel!
-    @IBOutlet var secondLabel: UILabel!
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
 
-    @IBOutlet var firstSlider: UISlider!
-    @IBOutlet var secondSlider: UISlider!
+    @IBOutlet weak var firstSlider: UISlider!
+    @IBOutlet weak var secondSlider: UISlider!
+
+    @IBInspectable
+    var title: String? {
+        get {
+            return propertyTitle.text
+        }
+        set {
+            propertyTitle.text = newValue
+        }
+    }
+
+    @IBInspectable
+    var firstTitle: String? {
+        get {
+            return firstLabel.text
+        }
+        set {
+            firstLabel.text = newValue
+        }
+    }
+
+    @IBInspectable
+    var secondTitle: String? {
+        get {
+            return secondLabel.text
+        }
+        set {
+            secondLabel.text = newValue
+        }
+    }
+
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return contentView.layer.cornerRadius
+        }
+        set {
+            contentView.layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    var viewBackgroundColor: UIColor? {
+        get {
+            return contentView.backgroundColor
+        }
+        set {
+            contentView.backgroundColor = newValue
+        }
+    }
+
+    @IBInspectable
+    var textTintColor: UIColor? {
+        get {
+            return propertyTitle.textColor
+        }
+        set {
+            propertyTitle.textColor = newValue
+            firstLabel.textColor = newValue
+            secondLabel.textColor = newValue
+            firstSlider.tintColor = newValue
+            secondSlider.tintColor = newValue
+        }
+    }
 
     func createNib() {
         let nib = UINib(nibName: "TastingPropertyView", bundle: Bundle(for: type(of: self)))
@@ -25,14 +89,23 @@ class TastingPropertyView: UIView {
         contentView.frame = self.bounds
     }
 
+    func setSlider(slider: UISlider) {
+        slider.minimumValue = 1
+        slider.maximumValue = 5
+        slider.isContinuous = false
+    }
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         createNib()
-
+        setSlider(slider: firstSlider)
+        setSlider(slider: secondSlider)
     }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         createNib()
+        setSlider(slider: firstSlider)
+        setSlider(slider: secondSlider)
     }
 }
