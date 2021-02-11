1abbf2b138d1ed65f5e2f2763bb33e2a9438af50
Martín Granados García <mgranados@users.noreply.github.com>
2021-02-11 08:42:06 -0600

    Improve NewBrew View and implemente PickerViewDelegate and DataSource for Brew Method. (#34)

--- PourOverflow/NewBrewViewController.swift
+++ PourOverflow/NewBrewViewController.swift
@@ -7,11 +7,32 @@
 
 import UIKit
 
-class NewBrewViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class NewBrewViewController: UIViewController {
+    @IBOutlet var brewMethodPicker: UIPickerView!
 
     @IBAction func cancelCreation(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
 
     var brewStore: BrewStore!
+
+    override func viewDidLoad() {
+        brewMethodPicker.dataSource = self
+        brewMethodPicker.delegate = self
+    }
+}
+
+extension NewBrewViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return BrewMethod.allCases.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return BrewMethod.allCases[row].rawValue
+    }
+
 }
