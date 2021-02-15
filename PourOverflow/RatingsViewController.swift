f08a5276d60de6809438dade665967ecc7a63877
Martín Granados García <mgranados@users.noreply.github.com>
2021-02-15 09:06:05 -0600

    Invisible footer view on RatingsView (#37)
    * Add invisible footer to the Rating view so the input can be viewed above keyboard easily.
    
    * Revert "Add invisible footer to the Rating view so the input can be viewed above keyboard easily."
    
    This reverts commit 459bc62186cb9393bedce5d7547321f500b51011.
    
    * Add show and dismiss keyboard in Ratings View

--- PourOverflow/RatingsViewController.swift
+++ PourOverflow/RatingsViewController.swift
@@ -8,11 +8,42 @@
 import UIKit
 
 class RatingsViewController: UIViewController {
+    @IBOutlet var scrollView: UIScrollView!
+
     @IBAction func dismissController(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
 
     @IBAction func showRatingInfo(_ sender: UIButton) {
         UIApplication.shared.open(URL(string: "https://www.youtube.com/watch?v=kEZZCQTSSAg")!, options: [:], completionHandler: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDissappear), name: UIResponder.keyboardWillHideNotification, object: nil)
+
+        let gestures = UITapGestureRecognizer(target: self.view, action: #selector(view.endEditing))
+        scrollView.addGestureRecognizer(gestures)
+    }
+
+    @objc func keyboardWillAppear(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else {
+            return
+        }
+        guard let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+            return
+        }
+        if self.view.frame.origin.y == 0 {
+            self.view.frame.origin.y -= keyboardFrame.cgRectValue.height
+        }
+
+    }
+
+    @objc func keyboardWillDissappear(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
     }
 }
